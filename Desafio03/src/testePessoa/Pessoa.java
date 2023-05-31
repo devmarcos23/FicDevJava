f3547216de9bdddbf58ab48787640113dd010d43
Marcos Vinicius <60987600+devmarcos23@users.noreply.github.com>
2023-05-31 10:46:09 -0400

    Add files via upload
    DayThree

---
+++ Desafio03/src/testePessoa/Pessoa.java
@@ -0,0 +1,26 @@
+
+package testePessoa;
+
+
+public class Pessoa {
+    private String nomePessoa;
+    private int idadePessoa;
+    
+    public void setNomePessoa(String nome){
+        this.nomePessoa = nome;
+    }
+    
+    public String getNomePessoa(){
+        return this.nomePessoa;
+    }
+    
+    public void setIdadePessoa(int idade){
+        this.idadePessoa = idade;
+    }
+    
+    public int getIdadePessoa(){
+        return this.idadePessoa;
+    }
+    
+    
+}
